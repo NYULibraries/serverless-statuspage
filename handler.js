761294225a2490f7886295c2075371c2af46a279
Elliott Grieco <elliott.grieco@gmail.com>
2018-06-12 15:08:47 -0400

    Makes stage-based headers testable

--- handler.js
+++ handler.js
@@ -2,25 +2,27 @@
 
 const statusjockey = require('status-jockey');
 const config = require('./config/sj-config.js');
-const headers = {
-  ["Access-Control-Allow-Origin"]: `https://${process.env.STAGE === 'prod' ? 'library.nyu.edu' : '*'}`,
-  ["Access-Control-Allow-Headers"]: "Content-Type"
-};
 
-module.exports.status = (event, context, callback) =>
-  Promise.resolve(event)
-    .then(() => statusjockey(event.queryStringParameters, config, process.env.STATUSPAGE_API_KEY))
-    .then(data => callback(null, {
+module.exports.status = (event, context, callback) => {
+  const headers = {
+    ["Access-Control-Allow-Origin"]: `${process.env.STAGE === 'prod' ? 'https://library.nyu.edu' : '*'}`,
+    ["Access-Control-Allow-Headers"]: "Content-Type"
+  };
+
+  return Promise.resolve(event)
+  .then(() => statusjockey(event.queryStringParameters, config, process.env.STATUSPAGE_API_KEY))
+  .then(data => callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(data),
+    headers
+  }))
+  .catch((err) => {
+    console.error(err.message);
+
+    callback(null, {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify([]),
       headers
-    }))
-    .catch((err) => {
-      console.error(err.message);
-
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify([]),
-        headers
-      });
     });
+  });
+};
